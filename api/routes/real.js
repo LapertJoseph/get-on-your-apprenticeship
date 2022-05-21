8c7a1de6c7047be86e0740685830ce0b119aada1
Lapertjoseph <lapertjoseph@gmail.com>
2022-05-21 18:56:12 +0200

    modified pour la 3eme star

--- api/routes/real.js
+++ api/routes/real.js
@@ -1,28 +1,54 @@
 var express = require("express");
 var router = express.Router();
-const axios = require('axios');
-const { response } = require('express')
+const axios = require("axios");
+const { response } = require("express");
 
-router.get("/", function (req, res,) {
-    res.send("This is real route");
+router.get("/", function (req, res) {
+  res.send("This is real route");
 });
 
 router.get("/students", async (req, res) => {
+  // route pour plus grande liste d'élèves
   try {
-    const response = await axios.get(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`)
-    res.send(response.data)  
+    const response = await axios.get(
+      `https://fedeperin-harry-potter-api-en.herokuapp.com/characters`
+    );
+    res.send(response.data);
   } catch (error) {
     console.log(error.message);
   }
 });
 
-router.get("/randomStudents", async(req, res) => {
+router.get("/randomStudents", async (req, res) => {
+  // route pour le random student (champion)
   try {
-    await axios.get(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`).then((response) => {
-      const longueur = response.data.length -1
-      const random = Math.floor(Math.random() * longueur );
-      res.json({data: response.data[random]})
-    })
+    await axios
+      .get(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`)
+      .then((response) => {
+        const longueur = response.data.length - 1;
+        const random = Math.floor(Math.random() * longueur);
+        res.json({ data: response.data[random] });
+      });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.get("/studentAppeal", async (req, res) => {
+  // route pour l'appel
+  try {
+    await axios
+      .get(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`)
+      .then((response) => response.data)
+      .then((response) => {
+        if (req.query.hogwartsHouse) {
+          const data = response.filter(
+            (student) => student.hogwartsHouse === req.query.hogwartsHouse
+          );
+          return res.json({ data });
+        }
+        return res.json({ data: response });
+      });
   } catch (error) {
     console.log(error.message);
   }
